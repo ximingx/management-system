d685c1128c85f9562933aaa5276d6b543306da6c
ximingx <ximing020714@126.com>
2023-06-04 20:23:28 +0800

    start

--- src/stores/main/system/user.ts
+++ src/stores/main/system/user.ts
@@ -1,7 +1,24 @@
 import { defineStore } from "pinia";
 import { addRequest, deleteRequest, editRequest, getRequest } from "@/services/modules/system";
 
-export const useSystemUserStore = defineStore("systemUser", {
+const orientSearchForm = {
+  name: "",
+  realname: "",
+  cellphone: "",
+  enable: 1,
+  createAt: ""
+};
+
+const orientForm = {
+  name: "",
+  realname: "",
+  password: "",
+  cellphone: "",
+  roleId: 0,
+  departmentId: 0
+};
+
+export const useSystemUserStore = defineStore("system-user", {
   state: () => {
     return {
       total: 0,
@@ -11,25 +28,12 @@ export const useSystemUserStore = defineStore("systemUser", {
         size: 10,
         offset: 0
       },
-      searchForm: {
-        name: "",
-        realname: "",
-        cellphone: "",
-        enable: 1,
-        createAt: ""
-      },
+      searchForm: Object.assign(orientSearchForm),
       dialog: {
         show: false,
         edit: false,
         editId: 0,
-        form: {
-          name: "",
-          realname: "",
-          password: "",
-          cellphone: "",
-          roleId: 0,
-          departmentId: 0
-        }
+        form: Object.assign(orientForm)
       },
       entireDepartment: [],
       entireRole: []
@@ -37,7 +41,7 @@ export const useSystemUserStore = defineStore("systemUser", {
   },
   actions: {
     // 获取查询用户列表
-    async getUserListAction() {
+    async getListAction() {
       const res = await getRequest("users", {
         offset: this.page.offset,
         size: this.page.size,
@@ -54,34 +58,13 @@ export const useSystemUserStore = defineStore("systemUser", {
       this.entireDepartment = department.data;
       this.entireRole = role.data;
     },
-    resetSearchForm() {
+    // 全部重置
+    resetAction() {
       this.$reset();
-      this.getUserListAction();
+      this.getListAction();
     },
-    // 新建用户
-    addAction() {
-      this.dialog.edit = false;
-      this.dialog.show = true;
-      this.dialog.form = {
-        name: "",
-        realname: "",
-        password: "",
-        cellphone: "",
-        roleId: 0,
-        departmentId: 0
-      };
-    },
-    // 删除用户
-    async deleteUserAction(id: number) {
-      const res = await deleteRequest("users", id);
-      ElMessage({
-        message: res.data,
-        type: res.code == "-1002" ? "error" : "success"
-      });
-      this.getUserListAction();
-    },
-    // 编辑用户
-    async handleEdit(row: any) {
+    // 点击编辑个人信息
+    async clickEditAction(row: any) {
       this.dialog.edit = true;
       this.dialog.editId = row.id;
       this.dialog.form = {
@@ -94,43 +77,42 @@ export const useSystemUserStore = defineStore("systemUser", {
       };
       this.dialog.show = true;
     },
-    // 提交编辑用户
-    async addUserAction() {
+    // 点击新建个人信息
+    clickAddAction() {
+      this.dialog.edit = false;
+      this.dialog.form = Object.assign(orientForm);
+      this.dialog.show = true;
+    },
+    async deleteAction(id: number) {
+      const res = await deleteRequest("users", id);
+      ElMessage({
+        message: res.data,
+        type: res.code == "-1002" ? "error" : "success"
+      });
+      this.getListAction();
+    },
+    async addAction() {
       if (this.dialog.edit) {
         await editRequest("users", this.dialog.editId, this.dialog.form);
       } else {
         await addRequest("users", this.dialog.form);
       }
-      this.getUserListAction();
+      this.getListAction();
       this.dialog.show = false;
-      this.dialog.form = {
-        name: "",
-        realname: "",
-        password: "",
-        cellphone: "",
-        roleId: 0,
-        departmentId: 0
-      };
+      this.dialog.form = Object.assign(orientForm);
     },
-    noAddUserAction() {
+    noAddAction() {
       this.dialog.show = false;
-      this.dialog.form = {
-        name: "",
-        realname: "",
-        password: "",
-        cellphone: "",
-        roleId: 0,
-        departmentId: 0
-      };
+      this.dialog.form = Object.assign(orientForm);
     },
-    handleSizeChange(e: any) {
+    sizeChangeAction(e: any) {
       this.page.offset = e;
-      this.getUserListAction();
+      this.getListAction();
     },
-    handleCurrentChange(e: any) {
+    currentChangeAction(e: any) {
       this.page.current = e;
       this.page.offset = (e - 1) * this.page.size;
-      this.getUserListAction();
+      this.getListAction();
     }
   }
 });
--- src/views/HomeView/MainSection/system/User.vue
+++ src/views/HomeView/MainSection/system/User.vue
@@ -2,76 +2,76 @@
 import { Refresh, Search } from "@element-plus/icons-vue";
 import { formatUTC } from "@/utils/day";
 import { useSystemUserStore } from "@/stores/main/system/user";
-const userSystemStore = useSystemUserStore();
-userSystemStore.getUserListAction();
-userSystemStore.getEntireAction();
+const systemUserStore = useSystemUserStore();
+systemUserStore.getListAction();
+systemUserStore.getEntireAction();
 </script>
 
 <template>
   <div class="user-box">
     <el-dialog
-      v-model="userSystemStore.dialog.show"
+      v-model="systemUserStore.dialog.show"
       title="用户信息"
       width="30%"
       center
       :show-close="false"
     >
-      <el-form v-model="userSystemStore.dialog.form">
+      <el-form v-model="systemUserStore.dialog.form">
         <el-form-item label="用户昵称" prop="name">
-          <el-input v-model="userSystemStore.dialog.form.name"></el-input>
+          <el-input v-model="systemUserStore.dialog.form.name"></el-input>
         </el-form-item>
         <el-form-item label="真实姓名" prop="realname">
-          <el-input v-model="userSystemStore.dialog.form.realname"></el-input>
+          <el-input v-model="systemUserStore.dialog.form.realname"></el-input>
         </el-form-item>
-        <el-form-item v-if="!userSystemStore.dialog.edit" label="登录密码" prop="password">
-          <el-input v-model="userSystemStore.dialog.form.password"></el-input>
+        <el-form-item v-if="!systemUserStore.dialog.edit" label="登录密码" prop="password">
+          <el-input v-model="systemUserStore.dialog.form.password"></el-input>
         </el-form-item>
         <el-form-item label="电话号码" prop="cellphone">
-          <el-input v-model="userSystemStore.dialog.form.cellphone"></el-input>
+          <el-input v-model="systemUserStore.dialog.form.cellphone"></el-input>
         </el-form-item>
         <el-form-item label="选择角色" prop="roleId">
-          <el-select v-model="userSystemStore.dialog.form.roleId">
-            <template v-for="item in userSystemStore.entireRole.list" :key="item.id">
+          <el-select v-model="systemUserStore.dialog.form.roleId">
+            <template v-for="item in systemUserStore.entireRole.list" :key="item.id">
               <el-option :label="item.name" :value="item.id"></el-option>
             </template>
           </el-select>
         </el-form-item>
         <el-form-item label="选择部门" prop="departmentID">
-          <el-select v-model="userSystemStore.dialog.form.departmentId">
-            <template v-for="item in userSystemStore.entireDepartment.list" :key="item.id">
+          <el-select v-model="systemUserStore.dialog.form.departmentId">
+            <template v-for="item in systemUserStore.entireDepartment.list" :key="item.id">
               <el-option :label="item.name" :value="item.id"></el-option>
             </template>
           </el-select>
         </el-form-item>
       </el-form>
       <div class="flex-box flex-center">
-        <el-button @click="userSystemStore.noAddUserAction">取消</el-button>
-        <el-button @click="userSystemStore.addUserAction">执行</el-button>
+        <el-button @click="systemUserStore.noAddAction">取消</el-button>
+        <el-button @click="systemUserStore.addAction">执行</el-button>
       </div>
     </el-dialog>
     <el-card>
       <template #header>
         <span>搜索</span>
       </template>
-      <el-form :model="userSystemStore.searchForm">
+      <el-form :model="systemUserStore.searchForm">
         <el-row :gutter="24" justify="space-between">
           <el-col :span="8">
             <el-form-item label="用户昵称" prop="name">
-              <el-input v-model="userSystemStore.searchForm.name"></el-input>
+              <el-input v-model="systemUserStore.searchForm.name"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item label="真实姓名" prop="realname">
-              <el-input v-model="userSystemStore.searchForm.realname"></el-input>
+              <el-input v-model="systemUserStore.searchForm.realname"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item label="电话号码" prop="cellphone">
-              <el-input v-model="userSystemStore.searchForm.cellphone"></el-input> </el-form-item
+              <el-input v-model="systemUserStore.searchForm.cellphone"></el-input> </el-form-item
           ></el-col>
           <el-col :span="8">
             <el-form-item label="个人状态" prop="enable">
-              <el-select v-model="userSystemStore.searchForm.enable" placeholder="选择查询状态">
+              <el-select v-model="systemUserStore.searchForm.enable" placeholder="选择查询状态">
                 <el-option label="正常" :value="1"></el-option>
                 <el-option label="禁用" :value="0"></el-option>
               </el-select>
@@ -80,7 +80,7 @@ userSystemStore.getEntireAction();
           <el-col :span="8"
             ><el-form-item label="创建时间" prop="createAt">
               <el-date-picker
-                v-model="userSystemStore.searchForm.createAt"
+                v-model="systemUserStore.searchForm.createAt"
                 type="daterange"
                 range-separator="-"
                 start-placeholder="开始时间"
@@ -90,17 +90,17 @@ userSystemStore.getEntireAction();
           <el-col :span="8"></el-col>
         </el-row>
         <div class="flex-box">
-          <el-button :icon="Refresh" @click="userSystemStore.resetSearchForm">重置</el-button>
-          <el-button :icon="Search" @click="userSystemStore.getUserListAction">查询</el-button>
+          <el-button :icon="Refresh" @click="systemUserStore.resetAction">重置</el-button>
+          <el-button :icon="Search" @click="systemUserStore.getListAction">查询</el-button>
         </div>
       </el-form>
     </el-card>
     <el-card>
       <template #header>
         <span>用户管理</span>
-        <el-button @click="userSystemStore.addAction">新建用户</el-button>
+        <el-button @click="systemUserStore.clickAddAction">新建用户</el-button>
       </template>
-      <el-table style="width: 100%" :data="userSystemStore.list">
+      <el-table style="width: 100%" :data="systemUserStore.list">
         <el-table-column align="center" type="selection" width="100" />
         <el-table-column align="center" type="index" label="序号" width="100" />
         <el-table-column align="center" prop="id" label="id" sortable width="100"></el-table-column>
@@ -133,20 +133,20 @@ userSystemStore.getEntireAction();
         </el-table-column>
         <el-table-column align="center" fixed="right" label="操作" width="140">
           <template #default="scope">
-            <el-button link @click="userSystemStore.deleteUserAction(scope.row.id)">删除</el-button>
-            <el-button link @click="userSystemStore.handleEdit(scope.row)">修改</el-button>
+            <el-button link @click="systemUserStore.deleteAction(scope.row.id)">删除</el-button>
+            <el-button link @click="systemUserStore.clickEditAction(scope.row)">修改</el-button>
           </template>
         </el-table-column>
       </el-table>
     </el-card>
     <el-pagination
-      v-model:current-page="userSystemStore.page.current"
-      v-model:page-size="userSystemStore.page.size"
+      v-model:current-page="systemUserStore.page.current"
+      v-model:page-size="systemUserStore.page.size"
       :page-sizes="[3, 5, 10, 25, 50, 100]"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="userSystemStore.total"
-      @size-change="userSystemStore.handleSizeChange"
-      @current-change="userSystemStore.handleCurrentChange"
+      :total="systemUserStore.total"
+      @size-change="systemUserStore.sizeChangeAction"
+      @current-change="systemUserStore.currentChangeAction"
     />
   </div>
 </template>
